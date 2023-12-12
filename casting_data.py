d1cec0e7f19d56edcabf3f52130d345c95080b8b
Rahmad1820 <150491783+Rahmad1820@users.noreply.github.com>
2023-12-11 18:11:19 -0800

    Add files via upload

---
+++ casting_data.py
@@ -0,0 +1,38 @@
+print('=======INT======')
+data_int = 9
+data_str = str(data_int)
+data_float = float(data_int)
+data_boolean = bool(data_int)
+print("data =",data_str,"type=",type(data_str))
+print("data =",data_float,"type=",type(data_float))
+print("data =",data_float,"type=",type(data_boolean))
+
+print('====STR====')
+data_str = 10
+data_float = float(data_str)
+data_int = int(data_str)
+data_boolean = bool(data_str)
+print("data=",data_float,"type=",type(data_float))
+print("data=",data_int,"type=",type(data_int))
+print("data=",data_boolean,"type=",type(data_boolean))
+
+print('====float====')
+data_float = 12
+data_int = int(data_float)
+data_str = str(data_float)
+data_boolean = bool(data_float)
+print("data=",data_int,"type=",type,(data_int))
+print("data=",data_str,"type=",type,(data_str))
+print("data=",data_boolean,"type=",type,(data_boolean))
+
+print('====boolean=====')
+data_boolean = 10
+data_str = str(data_boolean)
+data_float = float(data_boolean)
+data_int = int(data_boolean)
+print("data=",data_str,"type=",type(data_str))
+print("data=",data_float,"type=",type(data_float))
+print("data=",data_int,"type=",type(data_int))
+
+
+
